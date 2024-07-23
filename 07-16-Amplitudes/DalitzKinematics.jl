05fbd5c93ea479f8253ecd8b6764932ef1676092
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-07-23 17:35:50 +0200

    julia formatter

--- 07-16-Amplitudes/DalitzKinematics.jl
+++ 07-16-Amplitudes/DalitzKinematics.jl
@@ -7,7 +7,14 @@ using InteractiveUtils
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
     quote
-        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local iv = try
+            Base.loaded_modules[Base.PkgId(
+                Base.UUID("6e696c72-6542-2067-7265-42206c756150"),
+                "AbstractPlutoDingetjes",
+            )].Bonds.initial_value
+        catch
+            b -> missing
+        end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
@@ -17,19 +24,26 @@ end
 # ╔═╡ 023422ce-3f85-11ef-0185-19a1e34afd9e
 # ╠═╡ show_logs = false
 begin
-	using Pkg
-	Pkg.activate()
-	
-	using ThreeBodyDecays
-	using LinearAlgebra
-	using Plots
-	using Plots.PlotMeasures: mm
-	using PlutoUI
-	using LaTeXStrings
-	# 
-	theme(:wong2, frame=:box, grid=false, minorticks=true,
-	    guidefontvalign=:top, guidefonthalign=:right,
-	    foreground_color_legend=nothing, lab="")
+    using Pkg
+    Pkg.activate()
+
+    using ThreeBodyDecays
+    using LinearAlgebra
+    using Plots
+    using Plots.PlotMeasures: mm
+    using PlutoUI
+    using LaTeXStrings
+    #
+    theme(
+        :wong2,
+        frame = :box,
+        grid = false,
+        minorticks = true,
+        guidefontvalign = :top,
+        guidefonthalign = :right,
+        foreground_color_legend = nothing,
+        lab = "",
+    )
 end
 
 # ╔═╡ 4cd08b25-b024-49be-8919-36f2dd2af2ed
@@ -38,7 +52,7 @@ md"""
 """
 
 # ╔═╡ 5b45ab02-54de-41c4-98f9-917f4ac91373
-ms = ThreeBodyMasses(1,4,2; m0=18.0);
+ms = ThreeBodyMasses(1, 4, 2; m0 = 18.0);
 
 # ╔═╡ 958e46bc-b801-4548-80a4-45b316c48434
 md"""
@@ -47,48 +61,59 @@ md"""
 
 # ╔═╡ c1eb13f3-e827-49cd-9b1c-bcfc27ef8920
 function four_momenta(σs, ms)
-	ps = [breakup_Rk(σs[k], ms; k) for k in 1:3]
-	p1, p2, p3 = ps
-	
-	c12 = cosζ12_for0(σs, ms^2)
-	c31 = cosζ31_for0(σs, ms^2)
+    ps = [breakup_Rk(σs[k], ms; k) for k ∈ 1:3]
+    p1, p2, p3 = ps
 
-	E1, E2, E3 = sqrt.((ms^2)[1:3] .+ ps.^2)
-	# 
-	p1v = [0,0,-p1, E1]
-	p2v = [p2*sqrt(1-c12^2),0, -p2*c12, E2]
-	p3v = [-p3*sqrt(1-c31^2),0, -p3*c31, E3]
-	# 
-	@assert all((p1v+p2v+p3v)[1:3] .+ 1 .≈ 1.0) "p1v+p2v+p3v is not 0"
-	return p1v, p2v, p3v
+    c12 = cosζ12_for0(σs, ms^2)
+    c31 = cosζ31_for0(σs, ms^2)
+
+    E1, E2, E3 = sqrt.((ms^2)[1:3] .+ ps .^ 2)
+    #
+    p1v = [0, 0, -p1, E1]
+    p2v = [p2 * sqrt(1 - c12^2), 0, -p2 * c12, E2]
+    p3v = [-p3 * sqrt(1 - c31^2), 0, -p3 * c31, E3]
+    #
+    @assert all((p1v+p2v+p3v)[1:3] .+ 1 .≈ 1.0) "p1v+p2v+p3v is not 0"
+    return p1v, p2v, p3v
 end
 
 # ╔═╡ d4e66a78-26cb-4613-ab05-6d6e258dd05e
 function plot_momenta!(σs, ms; mc)
-	ps = four_momenta(σs, ms);
-	#
-	for (i,(p,m)) in enumerate(zip(ps, collect(ms)[1:3]))
-		plot!(sp=1, [0im,  dot(p, [1im, 0, 1, 0])], arrow=true,
-			lab=latexstring("\$p_$i\\,(m=$(round(Int, m)))\$"))
-	end
-	scatter!(sp=1, [0im]; ms=10, mc)
-	plot!(sp=1, xlab="", ylab="")
-	# 
-	annotate!(sp=1,
-		[(reim(dot(p,[1im, 0, 1, 0])/2)..., 
-				string(round(norm(p[1:3]), digits=1)), :bottom) for p in ps])
+    ps = four_momenta(σs, ms)
+    #
+    for (i, (p, m)) in enumerate(zip(ps, collect(ms)[1:3]))
+        plot!(
+            sp = 1,
+            [0im, dot(p, [1im, 0, 1, 0])],
+            arrow = true,
+            lab = latexstring("\$p_$i\\,(m=$(round(Int, m)))\$"),
+        )
+    end
+    scatter!(sp = 1, [0im]; ms = 10, mc)
+    plot!(sp = 1, xlab = "", ylab = "")
+    #
+    annotate!(
+        sp = 1,
+        [
+            (
+                reim(dot(p, [1im, 0, 1, 0]) / 2)...,
+                string(round(norm(p[1:3]), digits = 1)),
+                :bottom,
+            ) for p in ps
+        ],
+    )
 end
 
 # ╔═╡ be341733-eaaa-4950-ad1a-36bd4e20b353
-function plot_two(σs, ms; mc=2)
-	# 
-	plot(layout=grid(1,2), size=(700,350), aspect_ratio=1)
-	plot_momenta!(σs, ms; mc)
-	# 
-	plot!(sp=2, border13(ms), lw=2)
-	scatter!(sp=2, [σs.σ1], [σs.σ3], ms=10; mc)
-	# 
-	plot!(sp=2, xlab=L"m_{23}^2", ylab=L"m_{13}^2")
+function plot_two(σs, ms; mc = 2)
+    #
+    plot(layout = grid(1, 2), size = (700, 350), aspect_ratio = 1)
+    plot_momenta!(σs, ms; mc)
+    #
+    plot!(sp = 2, border13(ms), lw = 2)
+    scatter!(sp = 2, [σs.σ1], [σs.σ3], ms = 10; mc)
+    #
+    plot!(sp = 2, xlab = L"m_{23}^2", ylab = L"m_{13}^2")
 end
 
 # ╔═╡ 75c498ee-9345-48dd-a389-73fec3edc5df
@@ -97,13 +122,13 @@ md"""
 """
 
 # ╔═╡ 05750d26-626b-4ba2-b56d-996bbb0608b5
-@bind x Slider((0:0.01:1)[2:end-1], show_value=true, default=0.3)
+@bind x Slider((0:0.01:1)[2:end-1], show_value = true, default = 0.3)
 
 # ╔═╡ 121d754a-182d-4783-a90d-d7aa25d488e2
-@bind y Slider((0:0.01:1)[2:end-1], show_value=true, default=0.6)
+@bind y Slider((0:0.01:1)[2:end-1], show_value = true, default = 0.6)
 
 # ╔═╡ faada165-0e32-4922-9fcd-d851e7feb09f
-σ_test = x2σs([x,y], ms; k=1)
+σ_test = x2σs([x, y], ms; k = 1)
 
 # ╔═╡ 5caaa628-ee75-41f7-af6c-d58465622be2
 plot_two(σ_test, ms)
@@ -115,33 +140,37 @@ md"""
 
 # ╔═╡ 751c031f-a03e-4b8e-bd71-f0f32033f441
 xy_array = [
-	[0.9,0.9], [0.1,0.5],
-	[0.3,0.1], [0.9,0.3],
-	[0.7,0.7], [0.4,0.9],
-	[0.3,0.9], [0.3,0.5]
-	];
+    [0.9, 0.9],
+    [0.1, 0.5],
+    [0.3, 0.1],
+    [0.9, 0.3],
+    [0.7, 0.7],
+    [0.4, 0.9],
+    [0.3, 0.9],
+    [0.3, 0.5],
+];
 
 # ╔═╡ 06cc4774-d4c6-4295-89d3-80206c0a234a
 is_saved = false
 
 # ╔═╡ 6382f4cc-1781-4a7b-8f75-42f98584681b
 for (mc, xy) in enumerate(xy_array)
-	plot(aspect_ratio=1, size=(400,350))
-	σs = x2σs(xy, ms; k=2)
-	plot_momenta!(σs, ms; mc)
-	is_saved && savefig("~/Downloads/four_momenta_$mc.pdf")
+    plot(aspect_ratio = 1, size = (400, 350))
+    σs = x2σs(xy, ms; k = 2)
+    plot_momenta!(σs, ms; mc)
+    is_saved && savefig("~/Downloads/four_momenta_$mc.pdf")
 end
 
 # ╔═╡ cd553fc8-3a72-4d55-b3a0-7f0a923cf4fa
 let
-	plot(border13(ms), lw=2, lab="", aspect_ratio=1)
-	for (mc, xy) in enumerate(xy_array)
-		σs = x2σs(xy, ms; k=2)
-		scatter!([σs.σ1], [σs.σ3], ms=10; mc, lab="point $mc")
-	end
-	plot!()
-	savefig("~/Downloads/four_momenta_solution.pdf")
-	plot!(xlab=L"m_{23}^2", ylab=L"m_{13}^2")
+    plot(border13(ms), lw = 2, lab = "", aspect_ratio = 1)
+    for (mc, xy) in enumerate(xy_array)
+        σs = x2σs(xy, ms; k = 2)
+        scatter!([σs.σ1], [σs.σ3], ms = 10; mc, lab = "point $mc")
+    end
+    plot!()
+    savefig("~/Downloads/four_momenta_solution.pdf")
+    plot!(xlab = L"m_{23}^2", ylab = L"m_{13}^2")
 end
 
 # ╔═╡ Cell order:
--- 07-20-Amplitudes/DalitzProjections.jl
+++ 07-20-Amplitudes/DalitzProjections.jl
@@ -7,23 +7,30 @@ using InteractiveUtils
 # ╔═╡ f787599e-45c8-11ef-2dd4-61b67a34bcbd
 # ╠═╡ show_logs = false
 begin
-	using Pkg
-	Pkg.activate(".")
-	# 
-	using Plots
-	using Plots.PlotMeasures:mm
-	using Parameters
-	using JSON
-	using ThreeBodyDecaysIO
-	using ThreeBodyDecaysIO.ThreeBodyDecays
-	using ThreeBodyDecaysIO.HadronicLineshapes
-	# 
-	using QuadGK
-	using ThreeBodyDecaysIO
-	# 
-	theme(:wong2, frame=:box, grid=false, minorticks=true,
-	    guidefontvalign=:top, guidefonthalign=:right,
-	    foreground_color_legend=nothing, lab="")
+    using Pkg
+    Pkg.activate(".")
+    #
+    using Plots
+    using Plots.PlotMeasures: mm
+    using Parameters
+    using JSON
+    using ThreeBodyDecaysIO
+    using ThreeBodyDecaysIO.ThreeBodyDecays
+    using ThreeBodyDecaysIO.HadronicLineshapes
+    #
+    using QuadGK
+    using ThreeBodyDecaysIO
+    #
+    theme(
+        :wong2,
+        frame = :box,
+        grid = false,
+        minorticks = true,
+        guidefontvalign = :top,
+        guidefonthalign = :right,
+        foreground_color_legend = nothing,
+        lab = "",
+    )
 end
 
 # ╔═╡ 5da141a7-c68c-470f-9b5e-8083a8367b9d
@@ -45,18 +52,18 @@ $
 """
 
 # ╔═╡ 15da60bc-9dc6-48c4-95e0-15079822104f
-function project_cosθij_intergand(fs, ms, z; k) 
-	# 
-	i,j = ij_from_k(k)
-	mi, mj, mk, m0 = ms[i], ms[j], ms[k], ms[4]
-	# 
-	function integrand(xσk)
-		σs = x2σs([xσk, (z+1)/2], ms; k)
-		σk = σs[k]
-		jac = sqrt(Kallen(σk,mk^2,m0^2)*Kallen(σk,mi^2,mj^2))/σk
-		return fs(σs) / 2 * jac * ((m0-mk)^2-(mi+mj)^2)
-	end
-	return integrand
+function project_cosθij_integrant(fs, ms, z; k)
+    #
+    i, j = ij_from_k(k)
+    mi, mj, mk, m0 = ms[i], ms[j], ms[k], ms[4]
+    #
+    function integrand(xσk)
+        σs = x2σs([xσk, (z + 1) / 2], ms; k)
+        σk = σs[k]
+        jac = sqrt(Kallen(σk, mk^2, m0^2) * Kallen(σk, mi^2, mj^2)) / σk
+        return fs(σs) / 2 * jac * ((m0 - mk)^2 - (mi + mj)^2)
+    end
+    return integrand
 end
 
 # ╔═╡ 7a49d79e-73ba-4b61-8f13-00fa8b15f765
@@ -71,29 +78,29 @@ md"""
 
 # ╔═╡ 857440e6-f0e4-4e67-919d-60717acf512b
 begin
-	input = open(joinpath(@__DIR__, "x2pipipi-compass-1391643.json")) do io
-	    JSON.parse(io)
-	end
-	
-	workspace = Dict{String,Any}()
-	
-	@unpack functions = input
-	for fn in functions
-	    @unpack name, type = fn
-	    instance_type = eval(Symbol(type))
-	    workspace[name] = dict2instance(instance_type, fn)
-	end
-	
-	@unpack distributions = input
-	for dist in distributions
-	    @unpack name, type = dist
-	    instance_type = eval(Symbol(type))
-	    workspace[name] = dict2instance(instance_type, dist; workspace)
-	end
+    input = open(joinpath(@__DIR__, "x2pipipi-compass-1391643.json")) do io
+        JSON.parse(io)
+    end
+
+    workspace = Dict{String,Any}()
+
+    @unpack functions = input
+    for fn in functions
+        @unpack name, type = fn
+        instance_type = eval(Symbol(type))
+        workspace[name] = dict2instance(instance_type, fn)
+    end
+
+    @unpack distributions = input
+    for dist in distributions
+        @unpack name, type = dist
+        instance_type = eval(Symbol(type))
+        workspace[name] = dict2instance(instance_type, dist; workspace)
+    end
 end
 
 # ╔═╡ 71f12722-c866-4954-9f1c-ce93f1f28e83
-model_names = filter(x->occursin("compass", x), keys(workspace)) |> collect
+model_names = filter(x -> occursin("compass", x), keys(workspace)) |> collect
 
 # ╔═╡ e845a971-315c-4f03-9e3f-d9e6cdfc1bb0
 model_name = model_names[4]
@@ -110,16 +117,36 @@ md"""
 ms = model.chains[1].tbs.ms;
 
 # ╔═╡ 6f1f3684-e7a4-49ae-b144-0235f3855af5
-plot(layout=grid(1,3), size=(800,250),
-	plot(σs->σs[1], ms, c=cgrad(:roma, 10, categorical = true), aspect_ratio=1),
-	plot(σs->σs[2], ms, c=cgrad(:roma, 10, categorical = true), aspect_ratio=1),
-	plot(σs->σs[3], ms, c=cgrad(:roma, 10, categorical = true), aspect_ratio=1))
+plot(
+    layout = grid(1, 3),
+    size = (800, 250),
+    plot(σs -> σs[1], ms, c = cgrad(:roma, 10, categorical = true), aspect_ratio = 1),
+    plot(σs -> σs[2], ms, c = cgrad(:roma, 10, categorical = true), aspect_ratio = 1),
+    plot(σs -> σs[3], ms, c = cgrad(:roma, 10, categorical = true), aspect_ratio = 1),
+)
 
 # ╔═╡ b60e2811-75f8-43e4-8c81-8b119aa2bddc
-plot(layout=grid(1,3), size=(800,250),	
-	plot(σs->cosθ23(σs,ms^2), ms, c=cgrad(:roma, 10, categorical = true), aspect_ratio=1),
-plot(σs->cosθ12(σs,ms^2), ms, c=cgrad(:roma, 10, categorical = true), aspect_ratio=1),
-plot(σs->cosθ31(σs,ms^2), ms, c=cgrad(:roma, 10, categorical = true), aspect_ratio=1)
+plot(
+    layout = grid(1, 3),
+    size = (800, 250),
+    plot(
+        σs -> cosθ23(σs, ms^2),
+        ms,
+        c = cgrad(:roma, 10, categorical = true),
+        aspect_ratio = 1,
+    ),
+    plot(
+        σs -> cosθ12(σs, ms^2),
+        ms,
+        c = cgrad(:roma, 10, categorical = true),
+        aspect_ratio = 1,
+    ),
+    plot(
+        σs -> cosθ31(σs, ms^2),
+        ms,
+        c = cgrad(:roma, 10, categorical = true),
+        aspect_ratio = 1,
+    ),
 )
 
 # ╔═╡ 4bf7f8a2-bf99-4a32-b101-05393a01c6bd
@@ -127,54 +154,59 @@ I(σs) = unpolarized_intensity(model, σs)
 
 # ╔═╡ 7f3ba720-f43b-412c-87d0-a0f0fb5d162c
 begin
-	plot(ms, I; iσx = 3, iσy = 1, aspect_ratio=1)
-	plot!(border12(ms), l=(3, :black), aspect_ratio=1)
+    plot(ms, I; iσx = 3, iσy = 1, aspect_ratio = 1)
+    plot!(border12(ms), l = (3, :black), aspect_ratio = 1)
 end
 
 # ╔═╡ 25f5a348-6344-4527-b704-1363bb6d305f
 begin
-	p = plot(layout=grid(2,3), size=(800, 500),	yaxis=nothing,
-		map(1:3) do k
-			i,j = ij_from_k(k)
-			plot(0:0.03:2, xlab="m($i$j)") do σk
-				integrand = projection_integrand(I, ms, σk; k)
-				quadgk(integrand, 0, 1)[1]
-			end
-		end...,
-		map(1:3) do k
-			i,j = ij_from_k(k)
-			plot(shift_by_half(-1:0.03:1), xlab="cosθ($i$j)") do z
-				integrand = project_cosθij_intergand(I, ms, z; k) 
-				quadgk(integrand, 0, 1)[1]
-			end
-		end...
-	)
-	plot!(layout=grid(2,1, heights=(0.01,0.99)), size=(800, 500/0.9),
-		plot(title=model_name, xaxis=false, yaxis=false), p)
-	savefig("~/Downloads/dalitz_projections_$(model_name).pdf")
-	plot!()
+    p = plot(
+        layout = grid(2, 3),
+        size = (800, 500),
+        yaxis = nothing,
+        map(1:3) do k
+            i, j = ij_from_k(k)
+            plot(0:0.03:2, xlab = "m($i$j)") do σk
+                integrand = projection_integrand(I, ms, σk; k)
+                quadgk(integrand, 0, 1)[1]
+            end
+        end...,
+        map(1:3) do k
+            i, j = ij_from_k(k)
+            plot(shift_by_half(-1:0.03:1), xlab = "cosθ($i$j)") do z
+                integrand = project_cosθij_integrant(I, ms, z; k)
+                quadgk(integrand, 0, 1)[1]
+            end
+        end...,
+    )
+    plot!(
+        layout = grid(2, 1, heights = (0.01, 0.99)),
+        size = (800, 500 / 0.9),
+        plot(title = model_name, xaxis = false, yaxis = false),
+        p,
+    )
+    savefig("~/Downloads/dalitz_projections_$(model_name).pdf")
+    plot!()
 end
 
 # ╔═╡ 0252d2fa-cf26-4e8e-9176-805b392c3b52
 begin
-	plot(layout=(@layout [A B{0.8w,0.8h} ; D C]),
-		size=(700,700), link=:both)
-	plot!(sp=1, 0:0.01:2, permute=(:x, :y), yaxis=nothing, xlab="m²(12)") do σk
-				integrand = projection_integrand(I, ms, σk; k=1)
-				quadgk(integrand, 0, 1)[1]
-			end
-	plot!(sp=2, I, ms, iσx=1, iσy=3,
-			inset=(2,bbox(0.65,0.0,0.35,0.35)))
-	plot!(sp=3, xaxis=false, yaxis=false, top_margin=-10mm)
-	plot!(sp=4, 0:0.01:2, yaxis=nothing, xlab="m²(23)", top_margin=-10mm) do σk
-				integrand = projection_integrand(I, ms, σk; k=3)
-				quadgk(integrand, 0, 1)[1]
-			end
-	plot!(sp=2, xaxis=nothing, yaxis=nothing)
-	plot!(sp=5, 0:0.01:2, yaxis=nothing, xlab="m²(31)") do σk
-				integrand = projection_integrand(I, ms, σk; k=2)
-				quadgk(integrand, 0, 1)[1]
-			end
+    plot(layout = (@layout [A B{0.8w,0.8h}; D C]), size = (700, 700), link = :both)
+    plot!(sp = 1, 0:0.01:2, permute = (:x, :y), yaxis = nothing, xlab = "m²(12)") do σk
+        integrand = projection_integrand(I, ms, σk; k = 1)
+        quadgk(integrand, 0, 1)[1]
+    end
+    plot!(sp = 2, I, ms, iσx = 1, iσy = 3, inset = (2, bbox(0.65, 0.0, 0.35, 0.35)))
+    plot!(sp = 3, xaxis = false, yaxis = false, top_margin = -10mm)
+    plot!(sp = 4, 0:0.01:2, yaxis = nothing, xlab = "m²(23)", top_margin = -10mm) do σk
+        integrand = projection_integrand(I, ms, σk; k = 3)
+        quadgk(integrand, 0, 1)[1]
+    end
+    plot!(sp = 2, xaxis = nothing, yaxis = nothing)
+    plot!(sp = 5, 0:0.01:2, yaxis = nothing, xlab = "m²(31)") do σk
+        integrand = projection_integrand(I, ms, σk; k = 2)
+        quadgk(integrand, 0, 1)[1]
+    end
 end
 
 # ╔═╡ Cell order:
